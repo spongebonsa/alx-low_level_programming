66f2d1a466075adb398533d77bef5ab9677a221e
spongebonsa <spongebonsa145@gmail.com>
2022-12-09 05:40:24 -0800

    c needs to understand my errors too

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  prints all possible different combinations of two digits
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+		        if (x < y)
+		        {
+			        putchar(x);
+			        putchar(y);
+
+			        if (x != '8' || (x == '8' && y != '9'))
+			        {
+				       putchar(',');
+				       putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
