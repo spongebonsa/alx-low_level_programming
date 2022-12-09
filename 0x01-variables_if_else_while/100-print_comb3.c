4804bb692eedc30e9bf42688a39e135612b81a12
spongebonsa <spongebonsa145@gmail.com>
2022-12-09 14:33:28 -0800

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 	{
 		for (y = '0'; y <= '9'; y++)
 		{
-		        if (x < y)
-		        {
-			        putchar(x);
-			        putchar(y);
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
 
-			        if (x != '8' || (x == '8' && y != '9'))
-			        {
-				       putchar(',');
-				       putchar(' ');
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
