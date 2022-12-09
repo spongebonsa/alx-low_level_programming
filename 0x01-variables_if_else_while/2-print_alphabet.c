0850ca8bbbe09b361661649a931b96d2933cec72
spongebonsa <spongebonsa145@gmail.com>
2022-12-09 04:25:47 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ *
+ * Return: 0 if it exist
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
