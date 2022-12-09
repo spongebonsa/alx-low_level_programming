01a89a19427a7cf2b67865c77b9a9acf47c9cbf8
spongebonsa <spongebonsa145@gmail.com>
2022-12-09 14:24:28 -0800

    software is eating

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print possible combination of 2-digit
+ *
+ * Return: 0 if it exists
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b<= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (((c + d) > (a + b) && c >= a) || a < c)
+					{
+					putchar(a);
+				putchar(b);
+			putchar(' ');
+		putchar(c);
+	putchar(d);
+
+if (a + b + c + d == 227 && a == 57)
+{
+break;
+}
+else
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
