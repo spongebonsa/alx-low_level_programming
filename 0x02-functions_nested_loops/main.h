439ff85d1eb8fa6cefbe7df8affb8e734af771ad
spongebonsa <spongebonsa145@gmail.com>
2022-12-13 06:36:49 -0800

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN>H
+#ifndef MAIN_H
+#define MAIN_H
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,6 +11,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int, n);
+void print_to_98(int n);
 int _putchar(char c);
 #endif
