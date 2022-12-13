3eb6efc170eda232be25ed7584e6e72ccdf0f488
spongebonsa <spongebonsa145@gmail.com>
2022-12-13 08:06:30 -0800

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int i, j, k, l, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			l = k / 10;
+			m = k % 10;
+
+			if (j == o)
+			{
+				_putchar('0');
+			}
+			else if (k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(m + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(l + '0');
+				_putchar(m + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
