78962e953dcb697798fd130a944d9140c0d99393
spongebonsa <spongebonsa145@gmail.com>
2022-12-13 07:36:12 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: number
+ * Return: value of number
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
+}
