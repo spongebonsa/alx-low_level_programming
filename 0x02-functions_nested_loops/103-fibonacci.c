13471f1d2b50d953a510444ef6d3ee94a8b13a1b
spongebonsa <spongebonsa145@gmail.com>
2022-12-13 13:46:30 -0800

    prints number less than 4M

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints even number less than 4 million
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a = 0, b = 1, c = 0;
+	int add = 0;
+
+	while (c < 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0)
+			add += c;
+	}
+	printf("%i\n", add)
+		return (0);
+}
