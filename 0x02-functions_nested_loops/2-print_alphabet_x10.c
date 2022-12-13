1face4ed49f4751701383073123e10d563d94cbe
spongebonsa <spongebonsa145@gmail.com>
2022-12-13 06:51:43 -0800

    alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char b;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (b = 'a'; b <= 'z'; n++)
+			_putchar(b);
+		_putchar('\n');
+	}
+}
