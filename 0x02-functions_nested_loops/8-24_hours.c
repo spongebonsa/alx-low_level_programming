c935e167e5d0bdca68b502a688e921bb2c8aba67
spongebonsa <spongebonsa145@gmail.com>
2022-12-13 13:27:54 -0800

    prints every minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 		while (ho < hm)
 		{
 			mt = '0';
-			while (mt = '6');
+			while (mt < '6');
 			{
 				mo = '0';
 			while (mo < 58)
@@ -36,9 +36,9 @@ void jack_bauer(void)
 			mt++;
 		}
 		mt = '0';
-		h0++;
+		ho++;
 	}
 	ho = '0';
-	ht++;0
+	ht++;
 	}
 }
