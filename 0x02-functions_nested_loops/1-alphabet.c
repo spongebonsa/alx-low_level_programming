5ce5439aa48b180b13a9324a5daab0fbe8891280
spongebonsa <spongebonsa145@gmail.com>
2022-12-13 06:43:47 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets in lowercases
+ */
+void print_alphabet(void)
+{
+	char n;
+	
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+
+	_putchar('\n');
+}
