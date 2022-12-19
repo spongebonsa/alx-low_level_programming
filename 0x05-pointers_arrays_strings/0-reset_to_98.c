a7ae5e0d390548769df524c19fe808997ca41ddf
spongebonsa <spongebonsa145@gmail.com>
2022-12-19 12:10:55 -0800

    battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value of n to 9
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: something
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
